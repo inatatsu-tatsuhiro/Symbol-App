e40541397c6bbeaf228d21b7abf0e863deef16e7
inatatsu-tatsuhiro <tatsuhiro.9699@gmail.com>
2021-09-18 17:50:24 +0900

    :+1: add config file

--- src/pages/Home/Presenter.tsx
+++ src/pages/Home/Presenter.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
+import config from "utils/config";
+
 const Page: React.VFC = () => {
-  return <div>Home Page</div>;
+  return <div>Home Page　{config.SYMBOL_API_ENDPOINT}</div>;
 };
 
 export default Page;
